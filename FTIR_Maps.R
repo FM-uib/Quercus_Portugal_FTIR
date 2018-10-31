4f7a25546f1cabcc9eb4a47880d28b4796efdcae
FM-uib <florian.muthreich@uib.no>
2018-10-31 17:57:47 +0100

    working map with points and outline

--- FTIR_Maps.R
+++ FTIR_Maps.R
@@ -1,22 +1,35 @@
-setwd("O:/PhD/Data/Portugal 2018")
+
 library(ggplot2)
 library(rgdal)
 library(maptools)
 
-#Portugal Map
+# Portugal Map
+setwd("O:/PhD/Data/Portugal 2018")
 
 Portugal <- readOGR("countries/Iberian_Penin.shp")
 Portugal@data$id<-rownames(Portugal@data)
+
 Portugal.poly<-fortify(Portugal, region = "id", sort = F)
 
-Portugal.df<-merge(Portugal.poly,Portugal@data,by="id")
+Portugal.df<-merge(Portugal.poly, Portugal@data, by="id")
 
-Portugal.gg <- ggplot(data = Portugal.df, aes(x=long, y=lat, group = group, fill = SOVEREIGNT)) +
-  geom_polygon() + xlim(-9.5,-6) + ylim(36, 44) +
-  scale_fill_hue(l = 40) +
+Portugal.gg <- ggplot(data = Portugal.df) +
+  geom_path(aes(x=long, y=lat), color = "black") + 
+  xlim(-9.5,-6) + ylim(37, 42.1) +
   coord_equal(ratio = 1) +
-  theme_classic()
+  theme_classic() +
+  geom_point(data = subset, aes(x = Longitude, y = Latitude, 
+                                color = Sub_Spec, shape = Sub_Spec, size = 4)) +
+  scale_shape_manual(values = c(0:2,4:6))
+  
+print(Portugal.gg)
 theme(legend.position = "none", title = element_blank(),
       axis.text = element_blank())
 
-print(Portugal.gg)
+# Points of trees
+setwd("O:/PhD/Data/Portugal 2018/paper")
+
+load(file = "data.rda")
+
+ggplot(data = subset) +
+  geom_point(data = subset, aes(x = Longitude, y = Latitude, color = Sub_Spec))
