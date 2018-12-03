65e094cff1f7e453211fe8139f67a60a4a987071
FM-uib <florian.muthreich@uib.no>
2018-12-03 17:06:20 +0100

    workable version of the map finished

--- FTIR_Maps.R
+++ FTIR_Maps.R
@@ -1,4 +1,3 @@
-
 library(ggplot2)
 library(rgdal)
 library(maptools)
@@ -55,21 +54,32 @@ subset_ <- lat_lon_transform(x = subset., variable = "mean.lat", factor = "Sub_S
 subset_ <- lat_lon_transform(x = subset_, variable = "mean.long", factor = "Sub_Spec", 
                              offset = .15 * c(.5,.5,-.5,.5,-.5,-.5))
 
+subset_ <- lat_lon_transform(x = subset_, variable = "new.mean.lat", factor = "Group", 
+                             offset = .05 * c(0,-.5,0,0,0,0,0,0,-.5,0,0,0,0,0,1.5))
+subset_ <- lat_lon_transform(x = subset_, variable = "new.mean.long", factor = "Group", 
+                             offset = .05 * c(0,.5,0,0,0,0,0,0,-1.2,0,0,0,0,0,.5))
+
 ggplot(data = subset_) +
   geom_point(data = subset_, aes(x = new.mean.long, y = new.mean.lat, color = Sub_Spec, size = n))
 
 Portugal.gg <- ggplot(data = Portugal.df) +
   geom_path(aes(x=long, y=lat), color = "black") + 
-  xlim(-9.5,-6) + ylim(36.9, 42.2) +
+  xlim(-9.5,-6) + ylim(36.8, 42.2) +
   coord_equal(ratio = 1) +
-  theme_classic() +
-  geom_point(data = subset_, aes(x = new.mean.long, y = new.mean.lat, 
-                                color = Sub_Spec, shape = Section, size = n)) +
+  theme_bw() +
+  geom_point(data = subset_, aes(x = new.new.mean.long, y = new.new.mean.lat, 
+                                color = Sub_Spec, shape = Section, size = factor(n))) +
   scale_shape_manual(values = c(15:17)) +
-  scale_size(range = c(3,5))
+  scale_size_manual(values = c(rep(2,3),rep(3,3),rep(4,8)),breaks = c(3,5,7), labels = c("< 3","4 - 6","> 7")) +
+  labs(x = "Longitude", y = "Latitude", size = "# of Trees", shape = "Quercus Section", color = "Species")
+
+ggsave("O:/PhD/Data/Portugal 2018/paper/Sample_map.png", plot = Portugal.gg, device = "png", 
+       width = 15, height = 15, units = c("cm"), dpi = 600)
+ggsave("O:/PhD/Data/Portugal 2018/paper/Sample_map.svg", plot = Portugal.gg, device = "svg", 
+       width = 15, height = 15, units = c("cm"), dpi = 600)
 
 ggplot(data = subset_) +
-  geom_point(data = subset_, aes(x = new.mean.long, y = new.mean.lat, 
+  geom_point(data = subset_, aes(x = new.new.mean.long, y = new.new.mean.lat, 
                                  color = Sub_Spec, shape = Section, size = factor(n))) +
   coord_equal(ratio = 1) +
   scale_size_manual(values = c(rep(2,3),rep(3,3),rep(4,8)),breaks = c(3,5,7), labels = c("a","b","c"))
