d2b6c2149e9a4cfd5fb88298a3c0fa7463722382
FM-uib <florian.muthreich@uib.no>
2019-06-27 13:59:51 +0200

    finished Processing; started Env variable extraction

--- 01 Data Processing.R
+++ 01 Data Processing.R
@@ -25,17 +25,33 @@ SG_smooth <- function(spectra, deriv){
   library(EMSC)
   res = SavitzkyGolay(spectra, deriv = deriv)
   colnames(res) = colnames(spectra)
-  rownames(res) = rownames(spectra)
   return(res)
 }
 
 data$FTIR.SG2 <- SG_smooth(data$FTIR, deriv = 2)
 data$FTIR.SG1 <- SG_smooth(data$FTIR, deriv = 1)
+rownames(data$FTIR.SG1) <- data$ID
+rownames(data$FTIR.SG2) <- data$ID
 
-### Select six species of Quercus and create matrix for pls
+### Select six species of Quercus
 sp.filter <- c("broteroi", "robur", "estremadurensis", "coccifera", "rotundifolia", "suber")
 data <- data[data$Sub_Spec %in% sp.filter,]
 
-data$Sub_Spec <- factor(data$Sub_Spec, levels = sp.filter)
-data$Species.HO <- I(model.matrix(~Sub_Spec-1, data))
-colnames(data$Species.HO) <- str_sub(colnames(data$Species.HO), 9)
+### Mean the spectra by 
+mean_spectra <- function(data, ID){
+  tmp = as.matrix(do.call(rbind,lapply(unique(ID), function(x) colMeans(data[ID == x,]))))
+  return(tmp)
+}
+
+data_mean <- data[data$MRep == "03", c(1:13)]
+data_mean$FTIR = I(mean_spectra(data$FTIR, data$ID))
+data_mean$FTIR.SG2 = I(mean_spectra(data$FTIR.SG2, data$ID))
+data_mean$FTIR.SG2 = data_mean$FTIR.SG2[,c(1:5,data_mean$FTIR.SG2[1:5,(ncol(data_mean$FTIR.SG2)-4):ncol(data_mean$FTIR.SG2)])]
+
+### Create response matrix for pls
+data_mean$Sub_Spec <- factor(data_mean$Sub_Spec, levels = sp.filter)
+data_mean$Species.HO <- I(model.matrix(~Sub_Spec-1, data_mean))
+colnames(data_mean$Species.HO) <- str_sub(colnames(data_mean$Species.HO), 9)
+
+saveRDS(data, file = here("Data", "Output", "data.rds"))
+saveRDS(data_mean, file = here("Data", "Output", "data_mean.rds"))
