406d2c046072bc3e03acd6e1a4c4990d687e7caf
FM-uib <florian.muthreich@uib.no>
2019-08-20 22:20:39 +0200

    restored accidentaly removed function in 03_pls_analysis.R

--- R/functions/03_pls_analysis.R
+++ R/functions/03_pls_analysis.R
@@ -2,6 +2,32 @@ library(pls)
 library(MASS)
 library(caret)
 
+evaluate_pls <- function(npc, trained, data, train = TRUE){
+  error <- rep(0, npc)
+  con_M <- list()
+  if (train){
+    tested <- predict(trained, newdata = data[!data$train,], type = "score")
+    for (i in 1:npc) {
+      fitdata <- data.frame(Species = data$Sub_Spec[data$train], 
+                            FTIR.score = I(trained$scores[,1:i, drop = FALSE]))
+      testdata <- data.frame(Species = data$Sub_Spec[!data$train],
+                             FTIR.score = I(tested[,1:i,drop=FALSE]))
+      error[i] <- (nrow(tested) - sum(predict(lda(Species ~ FTIR.score, data = fitdata),
+                                              newdata = testdata)$class == testdata$Species))/nrow(tested)
+      con_M[[i]] <- I(confusionMatrix(predict(lda(Species ~ FTIR.score, data = fitdata), 
+                                              newdata = testdata)$class, testdata$Species))
+    }
+    result <- list("error" = error, "conf_matrix" = con_M, "fitdata" = fitdata, "testdata" = testdata)
+    return(result)
+  }else{
+    for (i in 1:npc) {
+      fitdata <- data.frame(Species = data$Sub_Spec, 
+                            FTIR.score = I(trained$scores[,1:i, drop = FALSE]))}
+    result <- list("fitdata" = fitdata)
+    return(result)
+  }
+}
+
 fold_pls <- function(data, folds, split = .6, npc = 20, pls = T){
   folds_res = list()
   for(i in 1:folds){
