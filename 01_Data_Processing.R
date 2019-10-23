d97e24e2817733b167a589741600f971e1e207ba
FM-uib <florian.muthreich@uib.no>
2019-10-23 16:21:59 +0200

    rewrote the Data Processing to conform with Boris precedure

--- 01_Data_Processing.R
+++ 01_Data_Processing.R
@@ -3,36 +3,37 @@ library(stringr)
 source(here("R", "functions", "01_Data_proc.R"))
 
 # Load Meta Info Dataframe
-meta_info <- readRDS(here("Data", "Input", "data_metainfo.rds"))
+meta_info <- read.csv(here("Data", "Input", "data_metainfo.csv"))
+meta_info = meta_info[order(meta_info$SID),]
+
+# Select Quercus Species for analysis
+sp.filter <- c("broteroi", "robur", "estremadurensis", "coccifera", "rotundifolia", "suber")
+data <- meta_info[meta_info$Sub_Spec %in% sp.filter,]
+data$Sub_Spec <- factor(data$Sub_Spec, levels = sp.filter)
 
 # Load Spectra
-spectra <- readRDS(here("Data", "Input", "Portugal_QuercusATR.rds"))
+spectra_raw <- read.csv(here("Data", "Input", "Quercus_ATR_RAW.csv"), check.names = F)
+spectra_raw$samples = sapply(spectra_raw$Filename, str_sub, start = -13L , end = -3L)
+spectra_raw = spectra_raw[order(spectra_raw$samples),]
+
+# Select spectra for analysis
+spectra_raw = spectra_raw[spectra_raw$samples %in% data$SID,]
+
+spectra_matrix = as.matrix(spectra_raw[,1:3318])
+wavenumbers = as.numeric(colnames(spectra_matrix))
+ftir = spectra_matrix[,wavenumbers <= 1900 & wavenumbers >= 700]
+ftir_sg = spectral_processing(spectra_matrix)
 
 # Add spectra to data
-meta_info$FTIR <- as.matrix(spectra[, 2:ncol(spectra)])
-data = meta_info
-
-rownames(data$FTIR) <- data$ID
-
-### Savitzky Golay derivative
-data$FTIR.SG2 <- SG_smooth(data$FTIR, deriv = 2)
-data$FTIR.SG1 <- SG_smooth(data$FTIR, deriv = 1)
-rownames(data$FTIR.SG1) <- data$ID
-rownames(data$FTIR.SG2) <- data$ID
-
-### Select six species of Quercus
-
-sp.filter <- c("broteroi", "robur", "estremadurensis", "coccifera", "rotundifolia", "suber")
-data <- data[data$Sub_Spec %in% sp.filter,]
+data$ftir = ftir
+data$ftir_sg = ftir_sg
 
 ### Mean the spectra by measurement
-data_mean <- data[data$MRep == "03", c(1:14)]
-data_mean$FTIR = I(mean_spectra(data$FTIR, data$ID))
-data_mean$FTIR.SG2 = I(mean_spectra(data$FTIR.SG2, data$ID))
-data_mean$FTIR.SG2 = data_mean$FTIR.SG2[,-c(1:5,(ncol(data_mean$FTIR.SG2)-4):ncol(data_mean$FTIR.SG2))]
+data_mean <- data[data$MRep == 03, c(2:15)]
+data_mean$ftir = I(mean_spectra(data$ftir, data$ID))
+data_mean$ftir_sg = I(mean_spectra(data$ftir_sg, data$ID))
 
 ### Create response matrix for pls
-data_mean$Sub_Spec <- factor(data_mean$Sub_Spec, levels = sp.filter)
 data_mean$Species.HO <- I(model.matrix(~Sub_Spec-1, data_mean))
 colnames(data_mean$Species.HO) <- str_sub(colnames(data_mean$Species.HO), 9)
 
--- R/functions/01_Data_proc.R
+++ R/functions/01_Data_proc.R
@@ -1,3 +1,20 @@
+spectral_processing <- function(spectra, poly = 2, width = 11, deriv = 2, scale = T){
+  #' Preprocess the data for Analysis.
+  #' 
+  #' @description Preprocess the raw spectra by calculating the second derivative using Savitzky Golay smoothing.
+  #' Spectra is also cropped to spectral region of interest 700 to 1900 cm-1.
+  #' @param spectra matrix. raw spectra to process
+  #' @return returns the second derivate and smoothed input spectra
+  #'
+  require(EMSC)
+  M = SavitzkyGolay(spectra, poly = poly, width = width, deriv = deriv)
+  if(scale) M = M * -1000 else M = M * -1
+  colnames(M) = colnames(spectra)
+  wavenumbers = as.numeric(colnames(M))
+  M = M[,wavenumbers <= 1900 & wavenumbers >= 700]
+  return(M)
+}
+
 SG_smooth <- function(spectra, deriv){
   library(EMSC)
   res = SavitzkyGolay(spectra, deriv = deriv, poly = 3, width = 11)
